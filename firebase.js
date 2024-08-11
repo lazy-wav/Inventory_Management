2319f98be78ec13115fb67cd85dea7ef0a55809b
s-sakshi9317 <io.sakshis9317@gmail.com>
2024-08-11 14:36:23 +0530

    Update globals.css, page.js; delete page.module.css; update package.json; add firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
